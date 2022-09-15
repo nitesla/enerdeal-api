a049f8d0f2fab422fbb34e3c1cc740c8960de192
Ariyo-Agbaje Gbeminiyi <gariyoagbaje@digicoreltd.com>
2022-09-15 08:50:39 +0100

    Email refactoring for amazon ses

--- src/main/java/com/enerdeal/service/NotificationService.java
+++ src/main/java/com/enerdeal/service/NotificationService.java
@@ -20,12 +20,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
 
 
 @SuppressWarnings("ALL")
@@ -73,7 +76,7 @@ public class NotificationService {
 
     @Value("${voice.otp.url}")
     private String voiceOtp;
-
+    static final String CONFIGSET = "ConfigSet";
     @Autowired
     private API api;
 
@@ -86,7 +89,60 @@ public class NotificationService {
     }
 
 
-    public void emailNotificationRequestq(NotificationRequestDto notificationRequestDto) {
+    public void emailNotificationRequest(NotificationRequestDto notificationRequestDto) {
+
+        try {
+        Properties props = System.getProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.port", mailSmtpPort);
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+
+        // Create a Session object to represent a mail session with the specified properties.
+        Session session = Session.getDefaultInstance(props);
+
+        // Create a message with the specified information.
+        MimeMessage msg = new MimeMessage(session);
+
+        msg.setFrom(new InternetAddress(mailFrom, mailSender));
+
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(notificationRequestDto.getMail()));
+
+        msg.setSubject(notificationRequestDto.getTitle());
+        msg.setContent(notificationRequestDto.getMessage(), "text/html");
+
+        // Add a configuration set header. Comment or delete the
+        // next line if you are not using a configuration set
+        msg.setHeader("X-SES-CONFIGURATION-SET", CONFIGSET);
+
+        // Create a transport.
+        Transport transport = session.getTransport();
+
+        // Send the message.
+            try {
+                System.out.println("Sending...");
+
+                // Connect to Amazon SES using the SMTP username and password you specified above.
+                transport.connect(mailHostName, mailUsername, mailPassword);
+
+                // Send the email.
+                transport.sendMessage(msg, msg.getAllRecipients());
+                System.out.println("Email sent!");
+            } catch (Exception ex) {
+                System.out.println("The email was not sent.");
+                System.out.println("Error message: " + ex.getMessage());
+            } finally {
+                // Close and terminate the connection.
+                transport.close();
+            }
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void emailNotificationRequestOption2(NotificationRequestDto notificationRequestDto) {
 
         ExecutorSingleton.getInstance().execute(() -> {
             try {
@@ -121,7 +177,7 @@ public class NotificationService {
 
     }
 
-    public NotificationResponseDto emailNotificationRequest (NotificationRequestDto notificationRequestDto){
+    public NotificationResponseDto emailNotificationRequestOption3 (NotificationRequestDto notificationRequestDto){
         Map<String,String> map = new HashMap();
         map.put("auth-key", authKey);
         map.put("fingerprint", uniqueId);
